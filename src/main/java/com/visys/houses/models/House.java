489c7a01ad6a52625af7a48d4f30ff740ed5888d
fernetsystem <fernetsystem@gmail.com>
2023-05-26 04:04:04 -0600

    Add @Entity Model House without setter and getter for lombok is not nesessary

--- src/main/java/com/visys/houses/models/House.java
+++ src/main/java/com/visys/houses/models/House.java
@@ -1,2 +1,30 @@
-package com.visys.houses.models;public class House {
+package com.visys.houses.models;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "houses")
+public class House {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", length = 150, nullable = false)
+    private String name;
+
+    @Column(name = "address", length = 500)
+    private String address;
+
+    @Column(name = "pictureMain", length = 1200)
+    private String pictureMain;
+
+    @Column(name = "price")
+    private Double price;
+
+
+
 }
